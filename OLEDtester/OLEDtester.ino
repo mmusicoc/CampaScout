6ba2e6838b9f36652f27535d019c3da7249a846c
Unknown <mariomusicortes@gmail.com>
2019-04-18 19:37:10 +0200

    Sketch para testear que el OLED funciona
    Carga a través de I2C un mensaje por defecto para comprobar que se visualiza correctamente.

---
+++ OLEDtester/OLEDtester.ino
@@ -0,0 +1,39 @@
+// Test for minimum program size.
+
+
+
+// 0X3C+SA0 - 0x3C or 0x3D
+
+#define I2C_ADDRESS 0x3C
+#define OLED_ENABLE     5
+
+#include <SSD1306Ascii.h>
+
+#include <SSD1306AsciiAvrI2c.h>
+
+
+
+SSD1306AsciiAvrI2c oled;
+
+//------------------------------------------------------------------------------
+
+void setup() {                
+
+  pinMode(OLED_ENABLE, OUTPUT);
+  digitalWrite(OLED_ENABLE, HIGH);
+
+  delay(1000);
+
+  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+
+  oled.setFont(System5x7);
+
+  oled.clear();
+
+  oled.print("Hello world!");
+
+}
+
+//------------------------------------------------------------------------------
+
+void loop() {}
