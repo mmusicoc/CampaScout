2eef6450b1dd9cfae91f5b84b30ac1108192f414
Unknown <mariomusicortes@gmail.com>
2019-04-18 21:10:23 +0200

    Controlador muy básico para bomba
    No cuenta ni guarda nada, solo controla la conmutación de la bomba en función del nivel de agua en el depósito.

---
+++ ControllerS/ControllerS.ino
@@ -0,0 +1,65 @@
+
+// PIN IDs ******************************************************************************************************************************
+
+// Pines
+#define SENSOR_MIN      A0                // En esta zona se debe configurar el funcionamiento del Arduino:
+#define SENSOR_MAX      A2                //    - Pines digitales: el número de pin donde se conecta cada cable.
+#define SWITCH_MODE     5                 //    - Pines analógicos: el número de pin precedido por "A".
+#define SWITCH_PUMP     7                 //    - Parámetros varios, como la velocidad de ejecución de las funciones de lectura (ahorro de energía).
+#define RELE            13
+#define LED             13
+
+// Parámetros del programa
+#define CORE_LOOP       30
+#define DELAY_LOOP      50
+#define LIMIT_SENSOR    1020
+#define CONTROL_SENSOR  50
+#define READ_MARGIN     20
+
+// Modo de uso I/O
+#define RELAY_ON        LOW
+#define RELAY_OFF       HIGH
+
+// DATA TYPES AND CONSTANTS *************************************************************************************************************
+
+enum level {underMin = 0, overMax = 1, midLevel = 2};     // Definición de tipo de dato que solo admite tres posibilidades de nivel de agua.
+
+// GLOBAL VARIABLES DECLARATIONS *******************************************************************************************************
+
+bool serial = true;                       // Guarda si está disponible y habilitado el puerto serial para transmisión de datos. Enviará mensajes.
+bool debug = false;                       // Variable que guarda si la depuración de errores está habilitada,
+                                          // enviará las funciones que se ejecutan y otros datos críticos.
+bool pumpStatus = false;                  // Guarda el estado actual de la bomba de llenado.
+level waterLevel;                         // Variable de tipo leve que guarda el estado actual del nivel de agua en el depósito.
+
+// SETUP *******************************************************************************************************************************
+
+void setup() {                            // Esta es la rutina estándar que se ejecuta cuando se reinicia el Arduino, es OBLIGATORIA. 
+  pinMode(SENSOR_MIN, INPUT);             // Activación de los pines I/O, definiendo si son de entrada (sensores) o salida (actuadores).
+  pinMode(SENSOR_MAX, INPUT);
+  pinMode(RELE, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(SWITCH_MODE, INPUT_PULLUP);     // Para los interruptores, los pines tendrán 2 posibles estados: +5V o flotante,
+  pinMode(SWITCH_PUMP, INPUT_PULLUP);     // y para evitar problemas el flotante se fuerza a un estado conocido.
+
+  digitalWrite(LED, LOW);                 // Se apaga el LED rojo antes de empezar, para asegurarnos de que conocemos su estado.
+  
+  serialStarter();                        // Llama a la función que inicia el puerto serial de transmisión de datos.
+  serialHandler();                        // Si está habilitado el puerto, envía el menú de configuración a la consola serial.
+}
+
+// LOOP ***********************************************************************************************************************************
+
+void loop() {                             // Una vez terminada la rutina "setup()", se ejecuta "loop()" indefinidamente hasta que se apague el Arduino.
+  delay(DELAY_LOOP);                      // Una pausa de DELAY_LOOP milisegundos para que no haga tantos bucles y gaste menos energía.
+  controlCore();                          // Dependiendo del estado del botón Auto/Manual, ejecuta la función principal de un modo distinto.
+  //Serial.print(digitalRead(SWITCH_MODE));
+  //Serial.println(digitalRead(SWITCH_PUMP));
+  //Serial.print(analogRead(SENSOR_MIN));
+  //Serial.print(" ");
+  //Serial.println(analogRead(SENSOR_MAX));
+  //Serial.print(digitalGetter(SWITCH_MODE, 30));
+  //Serial.println(digitalGetter(SWITCH_PUMP, 30));
+  
+}
+
---
+++ ControllerS/sensors.ino
@@ -0,0 +1,67 @@
+// PIN GETTERS **********************************************************************************************************************************
+
+bool analogGetter(uint8_t pin, byte control, int limit){       // Función que toma lecturas y hace las comparaciones.
+  byte valido = 0;
+  for (byte i = 0; i < control; i++){               // Hace "control" lecturas para asegurarse de que no hay falsas señales.
+    if (analogRead(pin) >= limit) valido++;         // Si la señal en "pin" es mayor o igual que "limit", anota la lectura como válida.
+    delayMicroseconds(READ_MARGIN);                          // Deja un intervalo de tiempo entre lecturas para que la señal se estabilice.
+  }
+  if (valido == control) return true;               // Si todas las lecturas han dado el mismo resultado satisfactorio,
+  else return false;                                // entonces confirma que la lectura es AFIRMATIVO (sin sentido físico).
+}
+
+bool digitalGetter(uint8_t pin, byte control){      // Función que toma lecturas y hace las comparaciones. Mismo algoritmo que "analogGetter()", pero en digital.
+  byte i;
+  byte valido = 0;
+  for (byte i = 0; i < control; i++){
+    if (digitalRead(pin) == HIGH) valido++;         // Si la señal digital es "HIGH", anota la lectura como válida.
+    delayMicroseconds(READ_MARGIN);
+  }
+  if (valido == control) return true;
+  else return false;
+}
+
+// SWITCHES HANDLER ****************************************************************************************************************************
+
+bool checkSwitchMode(){                             // Rutina que lee el estado del interruptor Auto/Manual.
+  if(serial) Serial.println();
+  FCN                                               // Macro de depuración, devuelve la función en la que estamos (ver pestaña "Debug").
+  if (digitalGetter(SWITCH_MODE, 10)) return false; // Si todas las lecturas han sido iguales, el modo es "true" (AUTO).
+  return true;
+}
+
+bool checkSwitchPump(){                             // Lo mismo que "checkSwitchMode()", pero para el botón de la bomba que se comprueba en modo Manual.
+  FCN
+  if (digitalGetter(SWITCH_PUMP, 10)) return false;
+  return true;
+}
+
+// WATER LEVEL SENSOR ****************************************************************************************************************************
+
+bool getSensorMin(){                                // Función que lee el sensor de nivel de agua inferior.
+  if (analogGetter(SENSOR_MIN, CONTROL_SENSOR, LIMIT_SENSOR)) return false;     // Si todas las lecturas han dado el mismo resultado satisfactorio,
+  else return true;                                 // entonces confirma que el nivel de agua está por DEBAJO del sensor. (así funciona la boya)
+}
+
+bool getSensorMax(){                                // Mismo algoritmo que "getSensorMin()", pero para el sensor de nivel de agua superior.
+  FCN
+  if (analogGetter(SENSOR_MAX, CONTROL_SENSOR, LIMIT_SENSOR)) return false;
+  else return true;
+}
+
+void getSensors(){                                  // Función que asigna a la variable waterLevel el estado del depósito.
+  FCN
+  if (getSensorMax()) {                             // Si la función "getSensorMax()" da positivo, el agua está por encima del sensor superior.
+    waterLevel = overMax;
+    if (serial) Serial.println("OVER MAX");
+  }
+  else if(getSensorMin()) {                         // En caso contrario, si la función "getSensorMin()" da positivo, 
+    waterLevel = midLevel;                          // el agua está por encima del sensor inferior, pero por debajo del superior: está a media altura.
+    if(serial) Serial.println("MIDDLE LEVEL");
+  }
+  else {                                            // Si no es ninguno de los casos anteriores, no queda más remedio que el bidón esté vacío y haya que rellenarlo.
+    waterLevel = underMin;
+    if(serial) Serial.println("UNDER MIN");
+  }
+}
+
